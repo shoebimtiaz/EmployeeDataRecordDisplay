e80915d01b2f010b1dcd5ba01e03c7d6c9dc5b55
shoebimtiaz <shoebimtiaz@gmail.com>
2020-03-17 17:40:31 -0500

    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeDataRecordDisplay.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connectionString = @"Data Source = LAPTOP-3KFTRPBK; Initial Catalog = HumanResourceDB; Integrated Security = True";
+        public ActionResult Index()
+        {
+            DataTable dataTableBook = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select EmployeeName, Department, ManagerName from Employee Join Manager On Employee.ManagerID = Manager.ID", connection);
+                dataAdapter.Fill(dataTableBook);
+            }
+            return View(dataTableBook);
+        }
+
+    }
+}
